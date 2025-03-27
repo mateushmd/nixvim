fc17eb369b0bbb67dd9c396bf3b93eb11b7dc4af
mateushmd <mateushmdiniz@gmail.com>
2025-03-26 22:09:23 -0300

    removing unused stuff

--- config/default.nix
+++ config/default.nix
@@ -1,11 +1,4 @@
 { lib, ... }:
-let
-  inherit (builtins) readDir;
-  inherit (lib.attrsets) foldlAttrs;
-  inherit (lib.lists) optional;
-
-  plugins = ./plugins;
-in
 {
   imports = [
       ./options.nix
