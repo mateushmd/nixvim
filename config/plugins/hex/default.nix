2e9090fb834ef9de2297e67a858bb2c1cbc18bb0
mateushmd <mateushmdiniz@gmail.com>
2025-07-16 21:34:07 -0300

    forgot to build

--- config/plugins/hex/default.nix
+++ config/plugins/hex/default.nix
@@ -1,5 +1,11 @@
 {
   plugins = {
-    hex.enable = true;
+    hex = { 
+      enable = true;
+      settings = {
+        assemble_cmd = "xxd -r";
+        dump_cmd = "xxd -g 1 -u";
+      };
+    };
   };
 }
