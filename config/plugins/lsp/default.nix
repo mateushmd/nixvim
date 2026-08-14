8500d060e053ca9df63f4d722bfd1623850a64e2
mateushmd <mateushmdiniz@gmail.com>
2026-08-14 02:00:21 -0300

    plugins/lsp: added nil

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -1,34 +1,57 @@
+{ pkgs, ... }: 
+let
+  envHost = builtins.getEnv "hostname";
+  host = if envHost != "" then envHost else "nixos";
+  nixosConfigPath = "${builtins.getEnv "HOME"}/repos/nixos";
+in 
 {
-  plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        asm_lsp.enable = true;
-        basedpyright.enable = true;
-        clangd = {
-          enable = true;
-          cmd = [
-            "clangd"
-            "--background-index"
-          ];
-          filetypes = [
-            "c"
-            "cpp"
-          ];
-        };
-        cssls.enable = true;
-        fish_lsp.enable = true;
-        html.enable = true;
-        lua_ls.enable = true;
-        nixd.enable = true;
-        rust_analyzer = {
-          enable = true;
-          installRustc = true;
-          installCargo = true;
-        };
-        svls.enable = true;
-        ts_ls.enable = true;
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      asm_lsp.enable = true;
+      basedpyright.enable = true;
+      clangd = {
+        enable = true;
+        cmd = [
+          "clangd"
+          "--background-index"
+        ];
+        filetypes = [
+          "c"
+          "cpp"
+        ];
       };
+      cssls.enable = true;
+      fish_lsp.enable = true;
+      html.enable = true;
+      lua_ls.enable = true;
+      nil_ls.enable = true;
+      nixd = {
+        enable = true;
+        /*
+        extraOptions = {
+          nixpkgs.expr = ''
+              import <nixpkgs> { }
+            '';
+          options = {
+            nixos.expr = ''
+              (builtins.getflake (builtins.tostring ${nixosConfigPath})).nixosconfigurations.${host}.options;
+            '';
+
+            nixvim.expr = ''
+              (builtins.getflake "github:nix-community/nixvim").nixvimconfigurations.${pkgs.system}.default.options;
+            '';
+          };
+        };
+        */
+      };
+      rust_analyzer = {
+        enable = true;
+        installRustc = true;
+        installCargo = true;
+      };
+      svls.enable = true;
+      ts_ls.enable = true;
     };
   };
 
@@ -41,6 +64,14 @@
       underline = {
         severity = vim.diagnostic.severity.ERROR,
       },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅙",
+          [vim.diagnostic.severity.WARN]  = "",
+          [vim.diagnostic.severity.INFO]  = "󰋼",
+          [vim.diagnostic.severity.HINT]  = "󰌵",
+        }
+      }
     })
 
     vim.api.nvim_create_autocmd("CursorHold", {
@@ -52,12 +83,5 @@
         })
       end,
     })
-
-    local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
-
-    for name, icon in pairs(symbols) do
-    	local hl = "DiagnosticSign" .. name
-    	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-    end
   '';
 }
